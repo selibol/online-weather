ed5f4266fb3034755e20832f50777474113902dc
Asus <selibol8@gmail.com>
2021-01-17 20:19:01 +0300

    fixed

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'weather_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'template')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
